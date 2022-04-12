418356de56e5338c752e76c132cf7481e6a1096a
Shera Amira <shera.rinaldy@gmail.com>
2022-04-12 22:35:11 +0700

    used grid for tracks layout, fixed some interfaces

--- src/component/card/card.jsx
+++ src/component/card/card.jsx
@@ -4,7 +4,7 @@ const Card =({item, onSelectTrack})=>{
     return(
       <div className="song-section">
         <div className="song-content">
-          <img className="album" id="albumImage" src={album.images[0].url} alt={songName} height="240"/>
+          <img className="album" id="albumImage" src={album.images[0].url} alt={songName} height="140"/>
           <div className="song-info">
             <p className="sTitle">{songName}</p>
             <p className="sArtist">{artists.map(artist => artist.name).join(', ')}</p>
